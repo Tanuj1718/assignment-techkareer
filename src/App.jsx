33bbd33af57b523c0fcf6ea1ffe1382c55bec2d4
Tanuj1718 <125687187+Tanuj1718@users.noreply.github.com>
2024-06-26 19:08:27 +0530

    Assignment Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,27 @@ import Button from "./components/Button"
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { BsSuitcaseLg } from "react-icons/bs";
-import { LuMessageSquare } from "react-icons/lu";
-import { RiHandCoinLine } from "react-icons/ri";
-import { GoDotFill } from "react-icons/go";
+import { LuMessageSquare, LuUserCheck } from "react-icons/lu";
+import { RiHandCoinLine, RiDeleteBin5Line } from "react-icons/ri";
+import { GoDotFill, GoPencil } from "react-icons/go";
 import DotIcon from "./components/dot";
 import { GrLocation } from "react-icons/gr";
 import { LiaCoinsSolid } from "react-icons/lia";
 import { CgFigma } from "react-icons/cg";
 import { SiAdobeillustrator, SiAdobexd } from "react-icons/si";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { FiMessageSquare } from "react-icons/fi";
+import { IoEyeOutline } from "react-icons/io5";
 
 function App() {
   return (
     
     <>
-      <div className="flex border-2 border-black w-[1728px] h-[1328px] bg-[#FCFCFC]">
-        <div className="w-[1728px] h-[1328px] top-[-700px] left-[-390px] border-2 border-black ">
-          <div className="h-[109px] w-[1728px]">
+      <div className="flex flex-wrap w-[1728px] h-[1328px] bg-[#FCFCFC] ">
+        <div className="w-[1728px] h-[1328px] bg-white">
+
+          <div className="h-[80px] w-[1728px]">
+            
             <div className="flex justify-between items-center h-[61px] w-[1664] mt-[24px] ml-[40px] mr-[30px] ">
               <div className="h-[61px] w-[100px] px-[28px] py-[15px] font-bold bg-[#E7E7E7]"><span className="hp[27px] w-[47px] font-[700px] text-[#DC4A2D]">Logo</span></div>
 
@@ -36,7 +41,7 @@ function App() {
                 </div>
                 </div>
               </div>
-              <div className="flex justify-between items-center h-[60px] w-[134px] px-[15px] py-[9px] gap-2">
+              <div className="flex justify-between items-center h-[60px] w-[134px] px-[15px] py-[9px] gap-2 ">
               <div className="flex justify-center items-center h-[32px] w-[32px]">
                   {/* icon bell */}
                   <IoIosNotificationsOutline className="size-[55px]" />
@@ -50,7 +55,8 @@ function App() {
               </div>
             </div>
           </div>
-          <div className="border-1 border-blue-900 h-[67px] w-[1728px]">
+          <hr />
+          <div className=" h-[67px] w-[1728px] mt-2 pt-4">
             <div className="h-[27px] w-[590px] flex justify-between ml-[100px] ">
               <span className="w-[117px] h-[27px] font-[600] text-[#DC4A2D]">Job Preview</span>
               <span className="w-[117px] h-[27px] font-[600] text-[#DC4A2D]">Application</span>
@@ -58,20 +64,22 @@ function App() {
               <span className="w-[117px] h-[27px] font-[600] text-[#DC4A2D]">Messages</span>
             </div>
           </div>
-
+          <hr />
           {/* third section */}
-          <div className="h-[1152px] w-[1728px] border-2 border-yellow-400">
+          <div className="h-[1050px] w-[1728px] flex justify-between ">
+            
+            <div className="">
             {/* whole container */}
-            <div className="h-[98px] w-[623px] ml-[100px] border-2 border-blue-600 gap-[12px]  flex flex-col ">
+            <div className="h-[98px] w-[623px] ml-[100px]  gap-[12px]  flex flex-col mt-4">
               <div className="h-[47px] w-[623px] flex justify-between items-center ">
-                <h1 className="h-[47px] w-[409px] font-[600] text-[#3D3D3D] text-[35px] border-2 border-black">Senior Product Designer</h1>
+                <h1 className="h-[47px] w-[409px] font-[600] text-[#3D3D3D] text-[35px] mt-3">Senior Product Designer</h1>
                 <DotIcon fill="gray" className="mt-4"/>
-                <span className="h-[19px] w-[117px] text-[14px] border-2 border-black mt-4 text-gray-500 ">posted 2 days ago</span>
-                <div className="h-[22px] w-57px] borde2 border-black">
-                  <Button icon={DotIcon} text="Open" textColor="green" className="border-green-400 py-2" />
+                <span className="h-[19px] w-[117px] text-[14px] mt-4 text-gray-500 ">posted 2 days ago</span>
+                <div className="h-[22px] w-57px]">
+                  <Button icon={DotIcon} text="Open" textColor="green" className="border-green-400 py-2 " />
                 </div>
               </div>
-              <div className=" h-[27px] w-[363px] mb-0 border-2 border-black flex items-center">
+              <div className=" h-[27px] w-[363px] mb-0 flex items-center">
                 <GrLocation className="size-[24px]"/>
                 <span className="text-[20px] font-[500] text-[#5D5D5D] ml-3">Delaware, USA</span>
                 <span className="ml-4"> <DotIcon fill="gray"/> </span>
@@ -80,7 +88,7 @@ function App() {
               </div>
             </div>
             <hr className="border-2 border-[#E7E7E7] h-[1px] w-[1319px] mt-[15px]"/>
-            <div className="h-[115px] w-[872px] ml-[100px] gap-[64px] border-2 border-blue-600 flex mt-[30px]">
+            <div className="h-[115px] w-[872px] ml-[100px] gap-[64px]  flex mt-[30px]">
               <div className=" flex flex-col h-[115px] w-[170px] gap-[6px] ">
                 <h3 className="text-[14px] font-[500] text-[#6E6D6D]">Skills Required</h3>
                 <div className="h-[88px] w-[126.41px] gap-[8px] flex flex-col  ">
@@ -98,17 +106,17 @@ function App() {
                   </div>
                 </div>
               </div>
-              <div className="h-[49px] w-[170px] gap-[4px] border-2 border-lime-600 flex flex-col">
+              <div className="h-[49px] w-[170px] gap-[4px] flex flex-col">
                 <span className="text-[#6E6D6D] text-[14px] font-[500]">Preferred Language</span>
                 <span className="text-[#3D3D3D
 ] text-16px] font-[600]">English</span>
               </div>
-              <div className="h-[49px] w-[170px] gap-[4px] border-2 border-lime-500 flex flex-col">
+              <div className="h-[49px] w-[170px] gap-[4px] flex flex-col">
               <span className="text-[#6E6D6D] text-[14px] font-[500]">Type</span>
                 <span className="text-[#3D3D3D
 ] text-16px] font-[600]">Full Time</span>
               </div>
-              <div className="h-[49px] w-[170px] gap-[4px] border-2 border-lime-500 flex flex-col">
+              <div className="h-[49px] w-[170px] gap-[4px] flex flex-col">
               <span className="text-[#6E6D6D] text-[14px] font-[500]">Years of Experience</span>
                 <span className="text-[#3D3D3D
 ] text-16px] font-[600]">3+ Years of Experience</span>
@@ -117,9 +125,9 @@ function App() {
             </div>
             <hr className="border-2 border-[#E7E7E7] h-[1px] w-[1319px] mt-[30px]"/>
             {/* content div  */}
-            <div className="h-[401px] w-[526px] ml-[100px] border-2 border-blue-700 mt-[30px] flex flex-col">
+            <div className="h-[401px] w-[526px] ml-[100px] mt-[30px] flex flex-col">
               <span className="text-[16px] font-[500] text-[#6E6D6D]">About the job</span>
-              <div className="flex flex-col border-2 border-y-lime-400 h-[374px] w-[526px] gap-[8px]">
+              <div className="flex flex-col h-[374px] w-[526px] gap-[8px]">
                 <ol className="text-[16px] font-[500] text[#3D3D3D]">
                   <li>1. Handle the UI/UX research design</li>
                   <li>2. Work on researching on latest web applications, designs & trends</li>
@@ -143,7 +151,88 @@ function App() {
                 <h4 className="text-[16px] font-[500] text[#3D3D3D]">Work location: In person</h4>
               </div>
             </div>
-            <h1>3rd main container</h1>
+            <hr className="border-2 border-[#E7E7E7] h-[1px] w-[1319px] mt-[15px]"/>
+            {/* Last section */}
+            <div className="h-[271px] w-[736px] ml-[100px] gap-[16px] flex flex-col mt-8">
+              <div className="h-[60px] w-[148px] flex p-[10px] pl-0 gap-[12px]">
+                <div className="size-[40px] rounded-[4px]"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr42lt0uL4-RkQGD5AoUhQGn4OGr-foggxTQ&s" alt="atlassian" /></div>
+                <span className="text-[20px] font-[500] text-[#4F4F4F] w-[86px] h-[27px]">Atlassian</span>
+              </div>
+              <div className="w-[736px] h-[195px] flex justify-between flex-wrap">
+                <div className="w-[344px] h-[49px] gap-[8px] flex flex-col">
+                  <span className="w-[92px] h-[19px] text-[14px] font-[500] text-[#6E6D6D]">Company size</span>
+                  <span className="text-[16px] font-[500] text-[#3D3D3D]">1k-2k Employees</span>
+                </div>
+                <div className="w-[344px] h-[49px] gap-[8px] flex flex-col">
+                <span className="w-[92px] h-[19px] text-[14px] font-[500] text-[#6E6D6D]">Type</span>
+                <span className="text-[16px] font-[500] text-[#3D3D3D]">Private</span>
+                </div>
+                <div className="w-[344px] h-[49px] gap-[8px] flex flex-col">
+                <span className="w-[92px] h-[19px] text-[14px] font-[500] text-[#6E6D6D]">Sector</span>
+                <span className="text-[16px] font-[500] text-[#3D3D3D]">Information Technology, Infrastructure</span>
+                </div>
+                <div className="w-[344px] h-[49px] gap-[8px] flex flex-col">
+                <span className="w-[92px] h-[19px] text-[14px] font-[500] text-[#6E6D6D]">Funding</span>
+                <span className="text-[16px] font-[500] text-[#3D3D3D]">Bootstrapped</span>
+                </div>
+                <div className="w-[344px] h-[49px] gap-[8px] flex flex-col">
+                <span className="w-[92px] h-[19px] text-[14px] font-[500] text-[#6E6D6D]">Founded in</span>
+                <span className="text-[16px] font-[500] text-[#3D3D3D]">2019</span>
+                </div>
+                <div className="w-[344px] h-[49px] gap-[8px] flex flex-col">
+                <span className="w-[92px] h-[19px] text-[14px] font-[500] text-[#6E6D6D]">Founded By</span>
+                <span className="text-[16px] font-[500] text-[#3D3D3D]">Scott Farquhar, Mike Cannon-Brooks</span>
+                </div>
+
+              </div>
+              
+            </div>
+
+            </div>
+            <div className="w-[409px] h-[1050px] mt-0 ml-[2px] flex flex-col">
+              <div className="flex h-[46px] w-[360px] gap-[16px] m-5">
+                {/* for buttons */}
+                <div className="h-[46px] w-[172px] rounded-[8px] border-[0.8px] border-[#DC4A2D] flex justify-center items-center">
+                <Button icon={RiDeleteBin5Line} text="Delete Job" className="font-[500] text-[16px] text-[#DC4A2D] border-none"/>
+                </div>
+                <div className="h-[46px] w-[172px] rounded-[8px] border-[0.8px] border-[#DC4A2D] flex justify-center items-center bg-[#DC4A2D]">
+                <Button icon={GoPencil} text="Edit Job" textColor="white" className="font-[500] text-[16px] text-white border-none"/>
+                </div>
+
+              </div>
+              <div className="h-[291px] w-[360px] p-[12px] m-5 mt-2 flex flex-col gap-[16px]">
+                {/* below div */}
+                <div className="h-[42px] w-[336px] flex justify-between">
+                  <div className="w-[135px] h-[42px] p-[10px] flex" >
+                      <HiOutlineUsers className="h-[20px] w-[20px] ml-[1.67px] mt-[2px]"/>
+                      <span className="h-[22px] w-[81px] font-[500] text-[16px] text-[#4F4F4F] ml-1">Applicants</span>
+                  </div>
+                  <div className="h-[39px] w-[57px] py-[6px] px-[10px] text-[20px] font-[600] text-[#3D3D3D]">400</div>
+                </div>
+                <div className="h-[42px] w-[336px] flex justify-between">
+                  <div className="w-[135px] h-[42px] p-[10px] flex" >
+                      <LuUserCheck className="h-[20px] w-[20px] ml-[1.67px] mt-[2px]"/>
+                      <span className="h-[22px] w-[81px] font-[500] text-[16px] text-[#4F4F4F] ml-1">Matches</span>
+                  </div>
+                  <div className="h-[39px] w-[57px] py-[6px] px-[10px] text-[20px] font-[600] text-[#3D3D3D]">100</div>
+                </div>
+                <div className="h-[42px] w-[336px] flex justify-between ">
+                  <div className="w-[135px] h-[42px] p-[10px] flex" >
+                      <FiMessageSquare className="h-[20px] w-[20px] ml-[1.67px] mt-[2px]"/>
+                      <span className="h-[22px] w-[81px] font-[500] text-[16px] text-[#4F4F4F] ml-1">Messages</span>
+                  </div>
+                  <div className="h-[39px] w-[57px] py-[6px] px-[10px] text-[20px] font-[600] text-[#3D3D3D]">147</div>
+                </div>
+                <div className="h-[42px] w-[336px] flex justify-between ">
+                  <div className="w-[135px] h-[42px] p-[10px] flex" >
+                      <IoEyeOutline className="h-[20px] w-[20px] ml-[1.67px] mt-[2px]"/>
+                      <span className="h-[22px] w-[81px] font-[500] text-[16px] text-[#4F4F4F] ml-1">Views</span>
+                  </div>
+                  <div className="h-[39px] w-[57px] py-[6px] px-[10px] text-[20px] font-[600] text-[#3D3D3D]">800</div>
+                </div>
+              </div>
+              
+            </div>
           </div>
         </div>
       </div>
